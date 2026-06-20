7f34e8391966c818f80c848083b9ad4360347401
shakil <me.mahadi.hasan.shakil@gmail.com>
2026-06-20 15:54:43 +0600

    solve challenge 4's step-2

--- 4. cut tool/main.c
+++ 4. cut tool/main.c
@@ -14,27 +14,41 @@ int main(int argc, char *argv[])
     if(filename[0] == '-' && filename[1] == 'f') {
         int col = atoi(filename + 2);
         if(col > 0) {
-            FILE *fp = fopen(*++argv, "r");
+            char *curargv = *++argv;
+            char delimiter = '\t'; /* default delimiter */
+            
+            if(curargv[0] == '-' && curargv[1] == 'd') { /* delimitar present */
+                delimiter = curargv[2];    
+                curargv = *++argv;
+            }
+
+            FILE *fp = fopen(curargv, "r");
 
             char buf[BUFSIZE];
             while(fgets(buf, BUFSIZE, fp) != NULL) {
                 char *p = buf;
                 int field = 1;
                 while(*p) {
-                    if(*p == ' ' || *p == '\t' || *p == '\n')
+                    if(delimiter == *p)
                         field++;
-                    else {
+                    else 
                         if(field == col) printf("%c", *p);
-                    }
+
                     p++;
                 }
-                printf("\n");
             }
         }
     }
 
     exit(0);
 }
+
+/* step 1: gcc main.c -o cut && ./cut -f2 data/sample.tsv && rm cut */
+// gcc main.c -o cut && ./cut -f5 data/sample.tsv | cat -e && rm cut
+
+/* step 2: gcc main.c -o cut && ./cut -f1 -d, data/fourchords.csv | head -n5 && rm cut */
+
+
 
 // gcc main.c -o cut && ./cut data/sample.tsv && rm cut
 // gcc main.c -o cut && ./cut -f2 data/sample.tsv && rm cut
