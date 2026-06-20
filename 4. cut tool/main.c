16e400fc84a4d7581a930b4ba7eebff9d383abd2
shakil <me.mahadi.hasan.shakil@gmail.com>
2026-06-20 18:12:23 +0600

    solved challenge 4's step 4

--- 4. cut tool/main.c
+++ 4. cut tool/main.c
@@ -6,11 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc < 3) {
-        perror("File path is missing.");
-        exit(1);
-    }
-
     char delimiter = '\t';
     unsigned int col_fetch = 0;
     int cval = 0;
@@ -42,24 +37,30 @@ int main(int argc, char *argv[])
 
    if(col_fetch > 0) {
         FILE *fp = fopen(filename, "r");
-        if(fp == NULL) {
-            fprintf(stderr, "error opending file: %s\n", filename);
-            exit(1);
-        }
+        if(fp == NULL)
+            fp = stdin;
 
         char buf[BUFSIZE];
         while(fgets(buf, BUFSIZE, fp) != NULL) {
             char *p = buf;
-            int field = 1;
+            int field = 1, pfch = 0;
             while(*p) {
+                if(*p == '\n') {
+                    ++p;
+                    continue;
+                }
+                
                 if(delimiter == *p) {
                     field++;
-                    if(*p != '\n' && (col_fetch & (1 << field)))
-                        putchar(delimiter);
+                    if(pfch && (col_fetch & (1 << field))) putchar(delimiter);
                 }
-                else 
-                    if(col_fetch & (1 << field)) printf("%c", *p);
-
+                
+                if(col_fetch & (1 << field)) {
+                    if(*p != delimiter) {
+                        putchar(*p);
+                        pfch = 1;
+                    }
+                }
                 p++;
             }
             putchar('\n');
@@ -78,6 +79,10 @@ int main(int argc, char *argv[])
 /* gcc main.c -o cut && ./cut -f"1 2, 3, 4" data/sample.tsv && rm cut */
 /* gcc main.c -o cut && ./cut -f1,2,3,4 -d, data/fourchords.csv | head -n5 && rm cut */
 // gcc main.c -o cut && ./cut -d, -f1,2,3,4 data/fourchords.csv | head -n5 && rm cut
+// gcc main.c -o cut && ./cut -f"1 2,3,4" <<< "hello world" && rm cut
+
+/* step 4 */
+// gcc main.c -o cut && tail -n5 data/fourchords.csv | ./cut -f"1 2" && rm cut
 
 // gcc main.c -o cut && ./cut data/sample.tsv && rm cut
 // gcc main.c -o cut && ./cut -f2 data/sample.tsv && rm cut
